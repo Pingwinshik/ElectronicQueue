a1c1e45b3d5232434eb7917d05f887722a371c6b
Lorenhow <CrashBuilder@yandex.ru>
2023-01-30 11:48:56 +0300

    A0.12
    Style HotFix!

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from dataclasses import dataclass
 import asyncio
 import os
 
+
 @dataclass
 class TData:
     type: str
@@ -12,13 +13,16 @@ class TData:
     operator_id: int
     status: int
 
+
 app = Sanic(__name__)
-app.static('/static', './static')
+app.static("/static", os.getcwd() + "/static")
+
 
 async def create_db_connection():
     Tickets = Database("postgresql://postgres:localhost:5432/Tickets")
     await Tickets.connect()
     return Tickets
+
 
 def init_db() -> None:
     async def _inner() -> None:
@@ -31,42 +35,51 @@ def init_db() -> None:
 	status SMALLINT NOT NULL
 };""")
 
+
 @app.route('/CreateTicket', methods=["POST"])
 async def create_ticket(data: TData) -> None:
     return()
 
+
 def init():
     app.run(host='127.0.0.1', port=9001, debug=True)
+
 
 @app.route('/')
 async def index(request):
   template = open(os.getcwd() + "/templates/index.html", encoding='utf-8')
   return html(template.read())
 
+
 @app.route("/login")
 async def login(request):
   template = open(os.getcwd() + "/templates/login.html", encoding='utf-8')
   return html(template.read())
+
 
 @app.route("/client")
 async def client(request):
   template = open(os.getcwd() + "/templates/client.html", encoding='utf-8')
   return html(template.read())
 
+
 @app.route('/ticket')
 async def ticket(request):
   template = open(os.getcwd() + "/templates/ticket.html", encoding='utf-8')
   return html(template.read())
+
 
 @app.route('/volunteer')
 async def volunteer(request):
   template = open(os.getcwd() + "/templates/volunteer.html", encoding='utf-8')
   return html(template.read())
 
+
 @app.route('/screen')
 async def screen(request):
   template = open(os.getcwd() + "/templates/screen.html", encoding='utf-8')
   return html(template.read())
+
 
 @app.route('/statistic')
 async def statistic(request):
